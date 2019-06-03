9a44a54760e01afdda5bd0a964fe863e61181748
imasicampo <47232111+imasicampo@users.noreply.github.com>
2019-06-03 09:33:32 -0700

    Update 6.3 9:30am
    Worked on documentation 2.0
    and other SCSS partials

--- Framework-SCSS/root/scripts/main.js
+++ Framework-SCSS/root/scripts/main.js
@@ -31,3 +31,18 @@ $('.nav a').on('click', function() {
   $('.menu').removeClass('active');
   $('.overlay').removeClass('menu-open');
 });
+
+// scroll top button
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 40) {
+      $('#topBtn').fadeIn();
+    } else {
+      $('#topBtn').fadeOut();
+    }
+  });
+
+  $('#topBtn').click(function(){
+    $('html, body').animate({ scrollTop: 0}, 800);
+  });
+});

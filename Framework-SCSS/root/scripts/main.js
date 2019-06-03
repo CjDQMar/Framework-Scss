ac8f38e2fffce94f4c82218114025a0a988de738
imasicampo <47232111+imasicampo@users.noreply.github.com>
2019-06-02 19:07:58 -0700

    Update 6.2.19 - Documentation.html
    Started "documentation.html"
    Added/Editted SCSS files

--- Framework-SCSS/root/scripts/main.js
+++ Framework-SCSS/root/scripts/main.js
@@ -21,3 +21,13 @@ $(window).on('scroll', function() {
     $('nav').removeClass('black');
   }
 });
+
+$('.menu').on('click', function() {
+  $(this).toggleClass('active');
+  $('.overlay').toggleClass('menu-open');
+});
+
+$('.nav a').on('click', function() {
+  $('.menu').removeClass('active');
+  $('.overlay').removeClass('menu-open');
+});
